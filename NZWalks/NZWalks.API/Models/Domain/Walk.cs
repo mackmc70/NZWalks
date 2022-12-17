c97fde8fad1decb212124168ebbe0b83ea87553a
Mike Mack <mackmc70@gmail.com>
2022-12-17 13:01:06 -0500

    End Of Section 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionID { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        // Navigation Properties
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
